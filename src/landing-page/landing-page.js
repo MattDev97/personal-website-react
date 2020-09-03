5b4fce3e1a03a76bab92d302d39f6dc9a20710dc
MattDev97 <mattpiano52@gmail.com>
2020-09-03 17:17:57 -0400

    Added Landing page

---
+++ src/landing-page/landing-page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './landing-page.css';
+
+class LandingPage extends React.Component {
+	render() {
+		return (
+			<div id="landing--container">
+				<div className="col my-auto">
+					<h1 className="mx-auto py-2">Matthew Muenzberg</h1>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default LandingPage;
