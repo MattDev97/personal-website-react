f6155e97bd4e892c1c7163a8bf7be609a752b364
MattDev97 <mattpiano52@gmail.com>
2020-10-04 14:07:28 -0400

    Added timeline

--- src/job-timeline/job-timeline.js
+++ src/job-timeline/job-timeline.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
-import JobDetailsJSON from '../assets/data/job-details.json';
+import TimelineJSON from '../assets/data/timeline.json';
 import './job-timeline.scss';
 
 class JobTimeline extends Component {
 
 	constructor(props) {
 		super(props);
-		this.timelineDetails = JobDetailsJSON;
+		this.timelineDetails = TimelineJSON;
+
+		this.constructTimelineMapByYear();
+	}
+
+	constructTimelineMapByYear() {
+		let timeline = TimelineJSON;
+		let map = {};
+		timeline.forEach(event => {
+			let startDate = new Date(event.StartDate);
+			let [s_month, s_date, s_year] = startDate.toLocaleDateString().split("/");
+
+			if (s_year in map) map[s_year] = [...map[s_year], event];
+			else map[s_year] = [event];
+
+			console.dir(map);
+			this.timelineMap = map;
+			this.years = Object.keys(map);
+		})
 	}
 
 	render() {
@@ -17,60 +35,33 @@ class JobTimeline extends Component {
 				<div className="row example-centered">
 					<div className="m-auto col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2">
 						<ul className="timeline timeline-centered">
-							{this.timelineDetails.map(job =>
-								(<li className="timeline-item">
-									<div className="timeline-info">
-										<span>{job.StartDate}</span>
-									</div>
-									<div className="timeline-marker"></div>
-									<div className="timeline-content">
-										<h3 className="timeline-title">{job.JobName}</h3>
-										<p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
-														eu pede mollis pretium. Pellentesque ut neque.</p>
-									</div>
-								</li>)
-							)}
+							{this.years.map(year => (
+								<React.Fragment>
+									<li className="timeline-item period" key={year}>
+										<div className="timeline-info"></div>
+										<div className="timeline-marker"></div>
+										<div className="timeline-content">
+											<h2 className="timeline-title">{year}</h2>
+										</div>
+									</li>
+									{this.timelineMap[year].map(event => (
+										<li className="timeline-item">
+											<div className="timeline-info">
+												<span>{event.StartDate}</span>
+											</div>
+											<div className="timeline-marker"></div>
+											<div className="timeline-content">
+												<h3 className="timeline-title">{event.Title}</h3>
+												{event.Description.map(paragraph => (
+													<p>{paragraph}</p>
+												))}
 
-							<li className="timeline-item">
-								<div className="timeline-info">
-									<span>March 23, 2016</span>
-								</div>
-								<div className="timeline-marker"></div>
-								<div className="timeline-content">
-									<h3 className="timeline-title">Event Title</h3>
-									<p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
-														eu pede mollis pretium. Pellentesque ut neque. </p>
-								</div>
-							</li>
-							<li className="timeline-item period">
-								<div className="timeline-info"></div>
-								<div className="timeline-marker"></div>
-								<div className="timeline-content">
-									<h2 className="timeline-title">April 2016</h2>
-								</div>
-							</li>
-							<li className="timeline-item">
-								<div className="timeline-info">
-									<span>April 02, 2016</span>
-								</div>
-								<div className="timeline-marker"></div>
-								<div className="timeline-content">
-									<h3 className="timeline-title">Event Title</h3>
-									<p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
-														eu pede mollis pretium. Pellentesque ut neque. </p>
-								</div>
-							</li>
-							<li className="timeline-item">
-								<div className="timeline-info">
-									<span>April 28, 2016</span>
-								</div>
-								<div className="timeline-marker"></div>
-								<div className="timeline-content">
-									<h3 className="timeline-title">Event Title</h3>
-									<p>Nullam vel sem. Nullam vel sem. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Donec orci lectus, aliquam ut, faucibus non, euismod id, nulla. Donec vitae sapien ut libero venenatis faucibus. ullam dictum felis
-														eu pede mollis pretium. Pellentesque ut neque. </p>
-								</div>
-							</li>
+											</div>
+										</li>
+									))}
+								</React.Fragment>
+
+							))}
 						</ul>
 					</div>
 				</div>
