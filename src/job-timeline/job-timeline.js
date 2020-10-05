e8434719734885bbcf6240a9c632252bcd4fa09a
MattDev97 <mattpiano52@gmail.com>
2020-10-05 16:58:58 -0400

    Added scss variables.  Added start date/end date on timeline

--- src/job-timeline/job-timeline.js
+++ src/job-timeline/job-timeline.js
@@ -8,8 +8,6 @@ class JobTimeline extends Component {
 
 	constructor(props) {
 		super(props);
-		this.timelineDetails = TimelineJSON;
-
 		this.constructTimelineMapByYear();
 	}
 
@@ -17,23 +15,36 @@ class JobTimeline extends Component {
 		let timeline = TimelineJSON;
 		let map = {};
 		timeline.forEach(event => {
-			let startDate = new Date(event.StartDate);
-			let [s_month, s_date, s_year] = startDate.toLocaleDateString().split("/");
-
-			if (s_year in map) map[s_year] = [...map[s_year], event];
-			else map[s_year] = [event];
-
-			console.dir(map);
-			this.timelineMap = map;
-			this.years = Object.keys(map);
+			let today = new Date();
+			let year = '';
+			if (event.EndDate && event.EndDate !== 'Present') {
+				console.log('End Date' + event.EndDate);
+				let endDate = new Date(event.EndDate);
+				year = endDate.toLocaleDateString().split("/")[2];
+			} else {
+				year = today.getFullYear().toString();
+			}
+			if (year in map) { map[year] = [...map[year], event]; console.log(year) }
+			else { map[year] = [event]; console.log(year) };
 		})
+		this.timelineMap = map;
+		console.dir(this.timelineMap);
+		this.years = Object.keys(map).slice().sort((a, b) => new Date(b) - new Date(a));
+		this.years.forEach(year => {
+			this.timelineMap[year] = this.timelineMap[year].slice().sort((a, b) => new Date(b.EndDate) - new Date(a.EndDate));
+			console.dir(this.timelineMap[year]);
+		})
+	}
+
+	sortByDate(timelineList) {
+		return timelineList.slice().sort((a, b) => new Date(b.StartDate) > new Date(a.StartDate))
 	}
 
 	render() {
 		return (
 			<div className="container-fluid pt-3">
 				<div className="row example-centered">
-					<div className="m-auto col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2">
+					<div className="m-auto col-md-10 col-xs-offset-1 col-sm-offset-2">
 						<ul className="timeline timeline-centered">
 							{this.years.map(year => (
 								<React.Fragment>
@@ -47,11 +58,20 @@ class JobTimeline extends Component {
 									{this.timelineMap[year].map(event => (
 										<li className="timeline-item">
 											<div className="timeline-info">
-												<span>{event.StartDate}</span>
+												<span className="timeline-startdate">From {event.StartDate}</span>
+												<span className="timeline-enddate">To {event.EndDate}</span>
 											</div>
 											<div className="timeline-marker"></div>
 											<div className="timeline-content">
 												<h3 className="timeline-title">{event.Title}</h3>
+												<h5 className="timeline-subtitle">{event.Subtitle}</h5>
+												<div className="timeline-tags my-1">
+													{event.Tags.map(tag => (
+														<div className="timeline-tag m-1 px-3">
+															{tag}
+														</div>
+													))}
+												</div>
 												{event.Description.map(paragraph => (
 													<p>{paragraph}</p>
 												))}
